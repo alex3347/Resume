ead1c3638870b37abde8d9481de7b0f91655aa75
Alex <821882886@qq.com>
2017-02-10 22:13:50 +0800

    modify gulpfile and change title style

--- gulpfile.js
+++ gulpfile.js
@@ -11,86 +11,92 @@ var gulp = require('gulp'),
 	cache = require('gulp-cache'),
 	rename = require('gulp-rename');
 
-gulp.task('sass', function(){
-  return gulp.src('src/scss/**/*.scss')
-  	.pipe(autoprefix())
-    .pipe(sass()) 
-    .pipe(gulp.dest('src/css'))
-//  .pipe(browserSync.reload({
-//    stream: true
-//  })
+gulp.task('sass', function() {
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(autoprefix({
+			browsers: ['last 5 versions', 'Android >= 4.0'],
+			cascade: true, //是否美化属性值 默认：true
+			remove: true //是否去掉不必要的前缀 默认：true 
+		}))
+		.pipe(gulp.dest('src/css'))
+		//  .pipe(browserSync.reload({
+		//    stream: true
+		//  })
 });
 
 gulp.task('browserSync', function() {
-  browserSync({
-//  server: {
-//    baseDir: ["dis", "font"],
-//    index: "../print.html"
-//  }
-	files: ["dis/css/*.css", "*.html"]
-  })
+	browserSync({
+		//  server: {
+		//    baseDir: ["dis", "font"],
+		//    index: "../print.html"
+		//  }
+		files: ["dis/css/*.css", "*.html"]
+	})
 });
 
-gulp.task('uglify', function () {
-    gulp.src('src/js/*.js') 
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify()) 
-    .pipe(gulp.dest('dis/js')); 
+gulp.task('uglify', function() {
+	gulp.src('src/js/*.js')
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('dis/js'));
 });
 
-gulp.task('minify-css', function () {
-    gulp.src('src/css/*.css') 
-    .pipe(rename({suffix: '.min'}))
-    .pipe(cleanCSS()) 
-    .pipe(gulp.dest('dis/css'));
+gulp.task('minify-css', function() {
+	gulp.src('src/css/*.css')
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('dis/css'));
 });
 
-
-gulp.task('minify-html', function () {
-    gulp.src('src/*.html')
-    .pipe(minifyHtml())
-    .pipe(gulp.dest('dis'));
+gulp.task('minify-html', function() {
+	gulp.src('src/*.html')
+		.pipe(minifyHtml())
+		.pipe(gulp.dest('dis'));
 });
 
-gulp.task('images', function(){
-  return gulp.src('src/img/**/*.+(png|jpg|jpeg|gif|svg)')
-  .pipe(cache(imagemin({
-      interlaced: true
-    })))
-  .pipe(gulp.dest('dis/img'))
+gulp.task('images', function() {
+	return gulp.src('src/img/**/*.+(png|jpg|jpeg|gif|svg)')
+		.pipe(cache(imagemin({
+			interlaced: true
+		})))
+		.pipe(gulp.dest('dis/img'))
 });
 
 gulp.task('del', function() {
-  return del.sync(['dis/**/*', '!dis/img', '!dis/img/**/*']);
+	return del.sync(['dis/**/*', '!dis/img', '!dis/img/**/*']);
 });
 
 gulp.task('del:all', function(callback) {
-  del('dis');
-  return cache.clearAll(callback);
+	del('dis');
+	return cache.clearAll(callback);
 })
 
-gulp.task('watch',['browserSync', 'sass'], function(callback){
-  gulp.watch('src/scss/**/*.scss', ['sass']));
-  gulp.watch('src/css/**/*.css', ['minify-css']));
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch('src/js/**/*.js', browserSync.reload);
+gulp.task('watch', ['browserSync', 'sass'], function(callback) {
+	gulp.watch('src/scss/**/*.scss', ['sass']);
+	gulp.watch('src/css/**/*.css', ['minify-css']);
+	gulp.watch('*.html', browserSync.reload);
+	gulp.watch('src/js/**/*.js', browserSync.reload);
 })
 
 gulp.task('default', function(callback) {
-  runSequence('sass', 'watch',
-    callback
-  )
+	runSequence('sass', 'watch',
+		callback
+	)
 })
 gulp.task('change-css', function(callback) {
-  runSequence('sass', 'minify-css',
-    callback
-  )
+	runSequence('sass', 'minify-css',
+		callback
+	)
 })
 gulp.task('build', function(callback) {
-  runSequence(
-    'del',
-    'sass',
-    ['uglify', 'minify-css', 'minify-html','images'],
-    callback
-  )
+	runSequence(
+		'del',
+		'sass',['uglify', 'minify-css', 'minify-html', 'images'],
+		callback
+	)
 })
